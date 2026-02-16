7fbe36b7b71e95745cca22fcba2af23afb681a90
plainionist <mail@plainionist.net>
2026-02-16 08:53:10 +0100

    builds

---
+++ src/Plainion.Graphs/CallerArgumentExpressionAttribute.cs
@@ -0,0 +1,16 @@
+// Polyfill for CallerArgumentExpressionAttribute which is not available in netstandard2.0
+#if !NET6_0_OR_GREATER
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    internal sealed class CallerArgumentExpressionAttribute : Attribute
+    {
+        public CallerArgumentExpressionAttribute(string parameterName)
+        {
+            ParameterName = parameterName;
+        }
+
+        public string ParameterName { get; }
+    }
+}
+#endif
